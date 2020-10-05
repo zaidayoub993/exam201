e19728a16aaec7144426dcec27d8c4851dbd84db
zaidayoub993 <zsayoub@gmail.com>
2020-10-05 14:23:21 +0300

    im fucked

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
-var flights = [];
+// ======================
+//      constructor function  
+// ======================
+var flights = []; //array 
 var Flight = function (from, to, capacity, reserved){
     this.from = from;
     this.to = to;
@@ -8,17 +11,23 @@ var Flight = function (from, to, capacity, reserved){
     this.available = 0 ; 
     flights.push(this);
 }
+// ======================
+//        capacity - reserved 
+// ======================
 Flight.prototype.getAvailabeSeats = function(){
 this.available = this.capacity - this.reserved;
 }
+//generateing new opjects (new)
 new Flight ('amman','aqaba', 30 , 12);
 new Flight ('tailand','jordan', 30 , 15);
 
-console.log(flights.length); 
+// console.log(flights.length); 
+// creating a table 
 var tableHD = ['from','to','capacity', 'reserved', 'available'];
 var sectionEl = document.getElementById('tablesection');
 var tableEl = document.createElement('table');
 sectionEl.appendChild(tableEl);
+//creating header for table 
 function tableH (){
     var trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
@@ -29,6 +38,9 @@ for ( var i = 0 ; i < tableHD.length ; i++){
 }
 }
 tableH();
+// ======================
+//      creating table row   
+// ======================
 Flight.prototype.render=function(){
     var trEl = document.createElement('tr');
     tableEl.appendChild(trEl);
@@ -48,11 +60,15 @@ Flight.prototype.render=function(){
     trEl.appendChild(tdEl);
     tdEl.textContent = this.available ;  
 }
-for ( var i = 0 ; i < flights.length ; i++){
-    flights[i].getAvailabeSeats();
-    flights[i].render();
+//calling the functions
+// for ( var i = 0 ; i < flights.length ; i++){
+//     flights[i].getAvailabeSeats();
+//     flights[i].render();
 
-}
+// 
+// ================
+// // the submit
+// ================ 
 var formEl=document.getElementById('form');
 formEl.addEventListener('submit', addFlight);
 function addFlight(event){
@@ -65,4 +81,42 @@ function addFlight(event){
     addFlight.getAvailabeSeats();
     addFlight.render();
     formEl.reset();
+    // ===========
+    // but it in localStorage
+    // ===========
+localStorage.setItem('flights',JSON.stringify (flights));
+tableEl.textContent = '';
+renderRows();
+}
+// ==============
+// get the loca Storage
+// ==============
+if(localStorage.getItem('flights')){
+    flights = [];
+    var localStorageData = JSON.parse(localStorage.getItem('flights'));
+ for (var i = 0 ; i < localStorageData.length; i++){
+     new Flight (localStorageData[i].from,localStorageData[i].to,localStorageData[i].capacity,localStorageData[i].reserved);
+ }
+}
+// ============
+// call the functions
+// ============
+function renderRows(){
+for ( var i = 0 ; i < flights.length ; i++){
+    flights[i].getAvailabeSeats();
+    flights[i].render();
+}
+total();
+}
+renderRows();
+// ===============
+// calc the total of the capacity 
+// ==============
+function total (){
+    var total= document.getElementById('totalCapacity');
+    var totalCapacity = 0 ;
+    for (var i = 0 ; i < flights.length ; i++){
+        totalCapacity+=Number(flights[i].capacity);
+    } 
+    total.textContent = `The Total Capacity is ${totalCapacity}`;
 }
